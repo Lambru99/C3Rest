262c1b9d4abe96889f80cbc428ada019038f3f47
Ludovico Latini <ludovico.latini@studenti.unicam.it>
2021-01-13 19:15:52 +0100

    prova in javafx

---
+++ src/main/java/it/unicam/cs/ids/papcteam/c3Rest/View/startController.java
@@ -0,0 +1,19 @@
+package it.unicam.cs.ids.papcteam.c3Rest.View;
+
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class startController {
+    public Button Login;
+    public Button registrazione;
+    public Label label;
+
+    public void login() {
+
+    }
+
+    public void registrazione() {
+
+    }
+}
